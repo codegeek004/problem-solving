cd23b61ca4fbe89a974e4426940cf6b53a45209a
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-09-16 21:36:41 +0530

    Iterative approach

--- factorial.cpp
+++ factorial.cpp
@@ -1,18 +1,15 @@
 #include<iostream>
 using namespace std;
-int fact(int n){
-    int res = 1;
-    for(int i=1;i<n;i++){
-        res = res*i;
-        cout << res << "fact" << endl;
-        return res;   
+int main(){
+    int n, fact=1;
+    cout << "Enter a number: " << endl;
+    cin >> n;
+    for(int counter = n; counter >=2; counter--){
+        cout << "counter: " << counter << endl;
+        fact = fact * counter;
+        cout << "fact: " << fact << endl;
     }
+    cout << "The factorial is: " << fact << endl;
     return 0;
 }
 
-int main(){
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    fact(n);
-}
