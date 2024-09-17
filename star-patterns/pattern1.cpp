a07018ae8929e2ec263adabf325b5bd0dea3a59b
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-09-17 23:46:46 +0530

    Initial commit

--- star-patterns/pattern1.cpp
+++ star-patterns/pattern1.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 using namespace std;
+//row count = column count
 int main(){
     cout << "Enter the value of n";
     int n;
     cin >> n;
-    
+//row count 
     int i = 1;
     while(i<=n){
+        //column count
         int j = 1;
         while(j<=n){
             cout << "*";
