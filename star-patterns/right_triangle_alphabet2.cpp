2862478efff3385aee64807d59210d57e8f3c51e
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-16 08:36:29 +0530

    right_triangle_alphabet2

---
+++ star-patterns/right_triangle_alphabet2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the value: ";
+    cin >> n;
+    int i = 0;
+    char ch = 'a';
+    while(i<=n){
+        int j = 0;
+        
+        while(j<=i){
+            cout << ch << " ";
+            ch = ch + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+
+    }
+}
