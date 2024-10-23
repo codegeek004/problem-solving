83d27be5d7e2a451b5af90204df1eed328139e9d
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-23 14:25:58 +0530

    prime number

--- prime.cpp
+++ prime.cpp
@@ -1,38 +1,23 @@
 #include<iostream>
 using namespace std;
-
-bool prime(int n)
-{
-    for(int i=2; i<=n; i++){
-        if(n%2 == 0){
-            cout << "Divisible by 2." << endl;
-            cout << "Not a prime" << endl;
-            return false;
-        }
-        else{
-            cout << "Not divisible by 2" << endl;
-            if(n%i==0){
-                cout << "Divisible by" << i << endl;
-                cout << "Not a prime" << endl;
-                return false;
-            }
-            else{
-                cout << "Not divisible by any number. " << endl;
-                cout << "It is prime";
-                return true;
-        
-            }
-        }
-    }
-    return 0;
-} 
-
 int main(){
 
     int n;
     cout << "Enter a value: ";
     cin >> n;
-    prime(n);
+    bool isPrime=0;
+    for(int i=2;i<n;i++){
+    if(n%i==0){
+        isPrime=1;
+        break;
+    }
+}
 
+    if(isPrime==0){
+        cout << "The number is prime" << endl;
+    }
+    else{
+        cout << "The number is not prime" << endl;
+    }
 
 }
