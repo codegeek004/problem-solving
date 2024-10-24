bde50274068763970dd464b5ce4211eafffb2557
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-24 11:22:42 +0530

    deleted precedence

--- precedence/pre_post_increment.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int i = 7;
-    cout << "++i: " << (++i) << endl;
-    cout << "i++: " << (i++) << endl;
-    cout << "i--: " << (i--) << endl;
-    cout << "--i: " << (--i) << endl;
-}
